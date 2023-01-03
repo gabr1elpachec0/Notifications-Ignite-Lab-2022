982c5a49070d249fcb2533488da8c4a13c6387f0
Gabriel Pacheco <gagopa@outlook.com>
2023-01-03 15:25:14 -0300

    Notification

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
+import { NotificationsRepository } from "@application/repositories/notifications-repository";
+import { PrismaNotificationsRepository } from "./prisma/repositories/prisma-notification-repository";
+
+@Module({
+    providers: [
+        PrismaService, 
+        {
+            provide: NotificationsRepository,
+            useClass: PrismaNotificationsRepository,
+        },
+    ],
+    exports: [
+        NotificationsRepository,
+    ],
+})
+
+export class DatabaseModule {}
